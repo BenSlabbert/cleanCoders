ef9d12f9fb1deac4034a2a227c4cef5b3e6cff12
Michael Fouche <mfouche@impactradius.com>
2016-08-23 13:45:10 +0200

    Read cli

--- src/main/java/Farm.java
+++ src/main/java/Farm.java
@@ -1,11 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by benjamin on 2016/08/23.
  */
 public class Farm {
     
     
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
         System.out.println("hello");
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        while(line!=null)
+        {
+            System.out.println(line);
+            line = br.readLine();
+        }
     }
 }
