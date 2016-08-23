34320147409c117e6c563c5cba71d0b05d30be00
Benjamin Slabbert <benjamin@impactradius.com>
2016-08-23 13:34:18 +0200

    adding sout

--- src/main/java/Farm.java
+++ src/main/java/Farm.java
@@ -1,4 +1,10 @@
 /**
  * Created by benjamin on 2016/08/23.
  */
-public class Farm {}
+public class Farm {
+    
+    
+    public static void main(String... args) {
+        System.out.println("hello");
+    }
+}
